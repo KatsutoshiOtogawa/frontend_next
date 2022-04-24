85727fd22496b2447f410eb50c17b08e5de72375
KatsutoshiOtogawa <gh1ab7im@gmail.com>
2022-04-24 15:46:56 +0900

    update

--- components/Prefecture.js
+++ components/Prefecture.js
@@ -4,27 +4,108 @@ import React from "react";
 // @material-ui/core components
 // import { makeStyles } from "@material-ui/core/styles";
 
-// import styles from "styles/jss/nextjs-material-kit/components/badgeStyle.js";
 
-// const useStyles = makeStyles(styles);
+/**
+ * @typedef {Object} PrefData - prefectureのデータ
+ * @property {number} prefCode - 県名コード
+ * @property {string} prefName - 県名
+ */
+
+
+/**
+* @typedef {Object} PrefectureJson - Prefectureのjson
+ * @property {string} message - SpecialTypeの文字列プロパティ
+ * @property {{prefCode: number; prefName: string;}[]} result - 県データ
+ */
 
 /**
  * @typedef {Object} Props - Prefectureのprops
- * @property {number} [size] - SpecialTypeの文字列プロパティ
+ * @property {PrefectureJson} data - SpecialTypeの文字列プロパティ
  */
 
 /**
- * 県データ
- * @param {Props} props
- * @returns {JSX.Element}
+ * @param {{data?: PrefectureJson}} props
+ * 
  */
-export function Prefecture(props) {
+export function Prefecture({data}) {
 
-  const [active, setActive] = React.useState();
-  const handleChange = (panel) => (event, expanded) => {
-    setActive(expanded ? panel : -1);
+  /** @type {PrefectureJson} */
+  data = {
+    message: null,
+    result: [
+      {
+        prefCode: null,
+        prefName: null
+      }
+    ]
   };
+  /** @type { React.Dispatch<React.SetStateAction<PrefectureJson>>} */
+  let setData
+  [data, setData] = React.useState(data);
+
+  React.useEffect(() => {
+
+    async () => {
+      const options = {
+        method: 'GET',
+        headers: { 
+        },
+      }
+      const response = await fetch(process.env.NEXT_PUBLIC_API_URL, options);
+
+      if (response.status != 200) {
+        console.error('fetch failed');
+        console.error(await response.text())
+        return;
+      }
+      /** @type {PrefectureJson} */
+      const json = await response.json()
+
+      setData(json);
+
+    };
+    // useEffectを一回だけ使いたいけど、eslinterのエラーが出るのでそれを消すために使う。
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  },[])
   return (
-    <p></p>
+
+    <>
+    {
+      data.result.map((value) => {
+        <>
+        <li>{value.prefCode}</li>
+        <li>{value.prefName}</li>
+        </>
+      })
+    }
+    </>
   );
+}
+
+export async function getStaticProps(context) {
+
+  /** @type {PrefectureJson} */
+  let data;
+
+  async () => {
+    const options = {
+      method: 'GET',
+      headers: { 
+      },
+    }
+    const response = await fetch(process.env.NEXT_PUBLIC_API_URL, options);
+
+    if (response.status != 200) {
+      console.error('fetch failed');
+      console.error(await response.text())
+      return;
+    }
+    data = await response.json();
+  };
+
+  return {
+    props: {
+      data
+    } // ページコンポーネントにpropsとして渡されます。
+  };
 }
--- pages/prefectures.js
+++ pages/prefectures.js
@@ -7,18 +7,12 @@ import React from 'react';
 import styles from '../styles/Home.module.css'
 import { Prefecture } from '../components/Prefecture'
 
-/**
- * @typedef {Object} Props - Prefectureのprops
- * @property {number} [size] - SpecialTypeの文字列プロパティ
- */
-
 
 /**
  * @typedef {Object} PrefData - prefectureのデータ
  * @property {number} prefCode - 県名コード
  * @property {string} prefName - 県名
  */
-
 
 /**
 * @typedef {Object} PrefectureJson - Prefectureのjson
@@ -27,77 +21,20 @@ import { Prefecture } from '../components/Prefecture'
  */
 
 /**
- * 
+ * @typedef {Object} Props - Prefectureのprops
+ * @property {PrefectureJson} data - SpecialTypeの文字列プロパティ
+ */
+
+/**
  * @returns {JSX.Element}
  */
 export default function Prefectures() {
 
-  /** @type {PrefectureJson} */
-  let data = {
-    message: null,
-    result: [
-      {
-        prefCode: null,
-        prefName: null
-      }
-    ]
-  };
-  /** @type { React.Dispatch<React.SetStateAction<PrefectureJson>>} */
-  let setData
-  [data, setData] = React.useState(data);
-
-  React.useEffect(() => {
-
-    (async function() {
-      const options = {
-        method: 'GET',
-        headers: { 
-        },
-      }
-      const response = await fetch(process.env.NEXT_PUBLIC_API_URL, options);
-
-      if (response.status != 200) {
-        console.error('fetch failed');
-        console.error(await response.text())
-        return;
-      }
-      /** @type {PrefectureJson} */
-      const json = await response.json();
-
-      setData(json);
-    }());
-
-    // useEffectを一回だけ使いたいけど、eslinterのエラーが出るのでそれを消すために使う。
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
-  
-  // callbackfn: (value: any, index: number, array: any[]) => void, thisArg?: any): void[]
-  // data.map((value) => {
-  // data.map((value) => {
-  //   console.log(value.prefName);
-  //   console.log(value.prefCode);
-
-
-//   const listItems = numbers.map((number) =>
-//   <li>{number}</li>
-// );
-
-  console.log(data)
+  // build時に一緒に値を出してしまう.
   return (
     <>
-    {
-      data.result
-    }
-    {/* <Prefecture /> */}
-    {
-      
-    data.result.map((value) => {
-      <>
-      <li>{value.prefCode}</li>
-      <li>{value.prefName}</li>
-      </>
-    })
-    }
+    <Prefecture />
+
     <p>
       HelloWorld
     </p>
